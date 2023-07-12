7510d752319483a07f3c65f3dae53544d34bf062
thumbthing <ilscc0902@gmail.com>
2023-07-12 23:36:42 +0900

    feat: 내비게이션 바의 필요 태그들 추가

--- app/components/NavigationBar.tsx
+++ app/components/NavigationBar.tsx
@@ -1,5 +1,29 @@
-import React from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { ChangeEvent, useState } from "react";
 
 export default function NavigationBar() {
-  return <div>NavigationBar</div>;
+  const router = useRouter();
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchText = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+
+    setSearchText(e.target.value);
+  };
+
+  return (
+    <div>
+      <button>menu</button>
+      <button onClick={() => router.push("/")}>react icon</button>
+      <input
+        type='text'
+        placeholder='Search'
+        onChange={(e) => handleSearchText(e)}
+      ></input>
+      <button>dark mode</button>
+      <p>profile</p>
+    </div>
+  );
 }
